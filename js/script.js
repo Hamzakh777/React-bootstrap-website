0a15a540da2bf7abd70ead22cdf5ea758385deed
Hamza <hamzabitup@gmail.com>
2018-07-17 13:59:38 +0100

    copying my project to github

---
+++ js/script.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.nav-bar',{
+          origin: 'bottom',
+          duration: 2000,
+});
+
+sr.reveal('.showcase-left',{
+          origin: 'top',
+          duration: 2000,
+          distance: '400px',
+
+});
+
+sr.reveal('.showcase-right',{
+          origin: 'right',
+          duration: 2000,
+          distance: '200px',
+});
+
+sr.reveal('.showcase-btn',{
+          origin: 'bottom',
+          duration: 2000,
+          delay:1000,
+});
+
+
+
